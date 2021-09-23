55f042bcb8bb3ef9cbab1f6678462d712ce90289
littleRex <little.ryrannosaurus@gmail.com>
2021-09-23 14:17:47 +0900

    add some service logic

--- src/main/java/com/crypto_tools/service/Service.java
+++ src/main/java/com/crypto_tools/service/Service.java
@@ -3,11 +3,24 @@ package com.crypto_tools.service;
 import com.crypto_tools.model.ExchangeMarketData;
 import com.crypto_tools.model.JsonFormatBean;
 
+import java.io.Closeable;
+import java.io.IOException;
 import java.util.List;
-
+@org.springframework.stereotype.Service
 public interface Service {
 
     List<JsonFormatBean> Arbitrage();
-    List<ExchangeMarketData> marketData();
+
+    List<ExchangeMarketData> binanceMarkeDataRest();
+
+    Closeable binanceMarketDataWebSocket(String sessionId) throws InterruptedException, IOException;
+
+    List<ExchangeMarketData> okexMarketDataRest();
+
+    Closeable okexMarketDataWebSocket(String sessionId) throws InterruptedException;
+
+    List<ExchangeMarketData> kuCoinMarketDataRest();
+
+    Closeable kuCoinMarketDataWebSocket(String sessionId) throws InterruptedException;
 
 }
--- src/main/java/com/crypto_tools/service/impl/Serviceimpl.java
+++ src/main/java/com/crypto_tools/service/impl/Serviceimpl.java
@@ -1,7 +1,20 @@
 package com.crypto_tools.service.impl;
 
 
+import com.crypto_tools.controller.WebSocketDispatcher;
+import com.crypto_tools.controller.config.SessionPool;
+import com.crypto_tools.exchangapi.ApiServiceGenerator;
+import com.crypto_tools.exchangapi.CallBack;
+import com.crypto_tools.exchangapi.binance.rest.Binance24hPriceResp;
+import com.crypto_tools.exchangapi.binance.websocket.BinanceAllMarketTickersResp;
+import com.crypto_tools.exchangapi.binance.websocket.BinanceWebSocketClient;
+import com.crypto_tools.exchangapi.kucoin.rest.KuCoinRestClient;
+import com.crypto_tools.exchangapi.kucoin.rest.allTickers.TickerData;
+import com.crypto_tools.exchangapi.kucoin.websocket.KuCoinMarketResp;
+import com.crypto_tools.exchangapi.kucoin.websocket.KuCoinWebSocketClient;
 import com.crypto_tools.exchangapi.okex.rest.OkexRestClient;
+import com.crypto_tools.exchangapi.okex.websocket.OkexTickersResp;
+import com.crypto_tools.exchangapi.okex.websocket.OkexWebSocketClient;
 import com.crypto_tools.model.ExchangeMarketData;
 import com.crypto_tools.model.JsonFormatBean;
 import com.crypto_tools.exchangapi.ExchangeFactory;
@@ -10,20 +23,17 @@ import com.crypto_tools.exchangapi.binance.rest.BinanceTickeResp;
 import com.crypto_tools.exchangapi.okex.rest.OkexTickerData;
 import com.crypto_tools.exchangapi.okex.rest.OkexTickerResp;
 import com.crypto_tools.service.Service;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.LinkedHashMap;
-import java.util.List;
+import java.io.Closeable;
+import java.io.IOException;
+import java.util.*;
 
 @org.springframework.stereotype.Service
-public class Serviceimpl implements Service {
+public class ServiceImpl implements Service {
 
-
-
-
-
-
+    private static Logger log = LoggerFactory.getLogger(ServiceImpl.class);
 
     @Override
     public List<JsonFormatBean> Arbitrage() {
@@ -131,28 +141,159 @@ public class Serviceimpl implements Service {
     }
 
     @Override
-    public List<ExchangeMarketData> marketData() {
-
+    public List<ExchangeMarketData> binanceMarkeDataRest() {
+        BinanceRestClient rest = ExchangeFactory.createExchangeFactory().createBinanceClients().createRestClient();
         List<ExchangeMarketData> mData = new ArrayList<>();
+        List<Binance24hPriceResp> restData = rest.get24hPrice();
+        for (int i = 0; i < restData.size(); i++) {
+            ExchangeMarketData exchangeMarketData = new ExchangeMarketData();
+            exchangeMarketData.setPair(restData.get(i).getSymbol());
+            exchangeMarketData.setLastprice(restData.get(i).getLastPrice());
+            exchangeMarketData.set_24hChanges(restData.get(i).getPriceChangePercent());
+            exchangeMarketData.set_24hVolume(restData.get(i).getVolume());
+            mData.add(exchangeMarketData);
+        }
+//        SessionPool.sendMessage(userId,mData.toString());
 
-        ExchangeFactory factory = ExchangeFactory.createExchangeFactory();
-        OkexRestClient rest = factory.createOkexClients().createRestClient();
+        return mData;
+    }
+
+    @Override
+    public Closeable binanceMarketDataWebSocket(String userId) throws InterruptedException, IOException {
+        ApiServiceGenerator.userId = userId;
+        BinanceWebSocketClient webSocket = ExchangeFactory.createExchangeFactory().createBinanceClients().createWebSocketClient();
+        List<ExchangeMarketData> mData = new ArrayList<>();
+        Closeable wb = webSocket.MarketTickersEvent(new CallBack<List<BinanceAllMarketTickersResp>>() {
+            @Override
+            public void onResponse(List<BinanceAllMarketTickersResp> var1) throws IOException, InterruptedException {
+                for (int i = 0; i < var1.size(); i++) {
+                    Thread.sleep(3000);
+                    System.out.println("币安");
+                    ExchangeMarketData exchangeMarketData = new ExchangeMarketData();
+                    exchangeMarketData.setPair(var1.get(i).getS());
+                    exchangeMarketData.setLastprice(var1.get(i).getC());
+                    exchangeMarketData.set_24hChanges(var1.get(i).getP());
+                    exchangeMarketData.set_24hVolume(var1.get(i).getV());
+                    mData.add(exchangeMarketData);
+                }
+                if(SessionPool.sessions.get(userId).isOpen()){
+                    SessionPool.sendMessage(userId, mData.toString());
+                }
+            }
+
+            @Override
+            public void onFailure(Throwable cause) {
+                log.info("binanceMarketDataWebSocketでエラー出ました、原因は{" + cause + "}");
+            }
+        });
+         return wb;
+    }
+
+    @Override
+    public List<ExchangeMarketData> okexMarketDataRest() {
+        OkexRestClient rest = ExchangeFactory.createExchangeFactory().createOkexClients().createRestClient();
+        List<ExchangeMarketData> mData = new ArrayList<>();
         OkexTickerData[] restData = rest.getAllTokenBaseData().getData();
 
         for (int i = 0; i < restData.length; i++) {
             ExchangeMarketData exchangeMarketData = new ExchangeMarketData();
             exchangeMarketData.setPair(restData[i].getInstId());
             exchangeMarketData.setLastprice(restData[i].getLast());
-            //need fix
-            exchangeMarketData.set_24hChanges(restData[i].getLast());
-            exchangeMarketData.set_24hHigh(restData[i].getHigh24h());
-            exchangeMarketData.set_24hLow(restData[i].getLow24h());
-            //need fix
-            exchangeMarketData.setMarketCap(restData[i].getHigh24h());
+            exchangeMarketData.set_24hChanges(String.valueOf((Double.valueOf(restData[i].getLast())
+                    - Double.valueOf(restData[i].getOpen24h())) /Double.valueOf(restData[i].getOpen24h()) * 100));
             exchangeMarketData.set_24hVolume(restData[i].getVol24h());
+            mData.add(exchangeMarketData);
         }
+//        SessionPool.sendMessage(userId,mData.toString());
 
-        return null;
+        return mData;
+    }
+
+    @Override
+    public Closeable okexMarketDataWebSocket(String userId) throws InterruptedException {
+        ApiServiceGenerator.userId = userId;
+        OkexWebSocketClient webSocket =  ExchangeFactory.createExchangeFactory().createOkexClients().createWebSocketClient();
+        List<ExchangeMarketData> mData = new ArrayList<>();
+        Closeable wb = webSocket.MarketTickersEvent(new CallBack<OkexTickersResp>() {
+            @Override
+            public void onResponse(OkexTickersResp var1) throws InterruptedException, IOException {
+                Thread.sleep(3000);
+                System.out.println("okex正在跑");
+                    OkexTickerData[] wsbData = var1.getData();
+                    for (int i = 0; i < wsbData.length; i++) {
+                        ExchangeMarketData exchangeMarketData = new ExchangeMarketData();
+                        exchangeMarketData.setPair(wsbData[i].getInstId());
+                        exchangeMarketData.setLastprice(wsbData[i].getLast());
+                        exchangeMarketData.set_24hChanges(String.valueOf((Double.valueOf(wsbData[i].getLast())
+                                - Double.valueOf(wsbData[i].getOpen24h())) /Double.valueOf(wsbData[i].getOpen24h()) * 100));
+                        exchangeMarketData.set_24hVolume(wsbData[i].getVol24h());
+                        mData.add(exchangeMarketData);
+                    }
+                if(SessionPool.sessions.get(userId).isOpen()){
+                    SessionPool.sendMessage(userId, mData.toString());
+                }else{
+                    System.out.println("ok已删除");
+//                    WebSocketDispatcher.closeWBClient_Exchange();
+                }
+            }
+
+            @Override
+            public void onFailure(Throwable cause) {
+                log.info("okexMarketDataWebSocketでエラー出ました、原因は{" + cause + "}");
+            }
+        });
+
+        return wb;
+
+    }
+
+    @Override
+    public List<ExchangeMarketData> kuCoinMarketDataRest() {
+        KuCoinRestClient rest = ExchangeFactory.createExchangeFactory().createKuCoinClients().createRestClient();
+        TickerData[] restData = rest.getAllTokenData().getData().getTicker();
+        List<ExchangeMarketData> mData = new ArrayList<>();
+        for (int i = 0; i < restData.length; i++) {
+            ExchangeMarketData exchangeMarketData = new ExchangeMarketData();
+            exchangeMarketData.setPair(restData[i].getSymbol());
+            exchangeMarketData.setLastprice(restData[i].getLast());
+            exchangeMarketData.set_24hChanges(restData[i].getChangeRate());
+            exchangeMarketData.set_24hVolume(restData[i].getVol());
+            mData.add(exchangeMarketData);
+        }
+//        SessionPool.sendMessage(userId,mData.toString());
+        return mData;
+    }
+
+    @Override
+    public Closeable kuCoinMarketDataWebSocket(String userId) throws InterruptedException {
+        ApiServiceGenerator.userId = userId;
+        KuCoinWebSocketClient webSocket = ExchangeFactory.createExchangeFactory().createKuCoinClients().createWebSocketClient();
+        Closeable wb = webSocket.Market(new CallBack<KuCoinMarketResp>() {
+            @Override
+            public void onResponse(KuCoinMarketResp var1) throws InterruptedException, IOException {
+                Thread.sleep(3000);
+                System.out.println("kucoin正在跑");
+                KuCoinMarketResp.MarketRespData.MarketData wsbdata = var1.getData().getData();
+                ExchangeMarketData exchangeMarketData = new ExchangeMarketData();
+                exchangeMarketData.setPair(wsbdata.getBaseCurrency()+wsbdata.getQuoteCurrency());
+                exchangeMarketData.setLastprice(wsbdata.getLastTradedPrice());
+                exchangeMarketData.set_24hChanges(wsbdata.getChangeRate());
+                exchangeMarketData.set_24hVolume(wsbdata.getVol());
+
+                if(SessionPool.sessions.get(userId).isOpen()){
+                    SessionPool.sendMessage(userId, exchangeMarketData.toString());
+                }else{
+                    System.out.println("kucoin已删除");
+//                    WebSocketDispatcher.closeWBClient_Exchange();
+                }
+            }
+
+            @Override
+            public void onFailure(Throwable cause) {
+                log.info("KuCoinWebSocketでエラーが出ました、原因は{" + cause + "}");
+            }
+        });
+        return wb;
     }
 
 
